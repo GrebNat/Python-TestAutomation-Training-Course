55c60c05017aa022416440511e9285df959f3c4c
Grebenshchikova, Natalia (Ext) <natalia.grebenshchikova_ext@novartis.com>
2021-06-03 12:25:16 +0300

    hw8 task202

--- Lecture_9_Testing_homework/hw2.py
+++ Lecture_9_Testing_homework/hw2.py
@@ -1,8 +1,58 @@
 """
 Write a context manager, that suppresses passed exception.
 Do it both ways: as a class and as a generator.
->>> with supressor(IndexError):
-...    [][2]
+# >>> with supressor(IndexError):
+# ...    [][2]
 """
 
-# тут я совсем не поняла, что требуется
+
+class Suppress:
+    def __init__(self, e):
+        self.e = e
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if type == self.e:
+            return self
+
+
+def suppressed():
+    with Suppress(ValueError):
+        int('string')
+
+
+suppressed()
+#########################################
+
+
+class Suppress2:
+    def __init__(self, e: type):
+        self.e = e
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if type == self.e:
+            return self
+
+
+def contextmanager(func):
+    def helper(e: type):
+        return Suppress2(func(e))
+
+    return helper
+
+
+@contextmanager
+def s(e: type):
+    return e
+
+
+with s(ZeroDivisionError) as err:
+    1 / 0
+
